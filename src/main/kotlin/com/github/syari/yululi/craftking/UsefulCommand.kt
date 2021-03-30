5717cea3ad1b64be7e2936c4885daad55eecc04b
sya-ri <sya79lua@gmail.com>
2021-03-30 22:07:02 +0900

    /help /point を追加

--- src/main/kotlin/com/github/syari/yululi/craftking/UsefulCommand.kt
+++ src/main/kotlin/com/github/syari/yululi/craftking/UsefulCommand.kt
@@ -2,12 +2,31 @@ package com.github.syari.yululi.craftking
 
 import com.github.syari.spigot.api.command.command
 import com.github.syari.yululi.craftking.Main.Companion.plugin
+import com.github.syari.yululi.craftking.PointCounter.Companion.craftPointCounter
+import org.bukkit.entity.Player
 
 object UsefulCommand {
     fun register() {
+        plugin.command("help") {
+            execute {
+                sender.send(
+                    """
+                        &fコマンド一覧
+                        &a/pickup &7ピックアップアイテムを確認します
+                        &a/point &7現在のポイントを確認します
+                    """.trimIndent()
+                )
+            }
+        }
         plugin.command("pickup") {
             execute {
                 sender.spigot().sendMessage(PointCalculator.pickUpMessage)
+            }
+        }
+        plugin.command("point") {
+            execute {
+                val player = sender as? Player ?: return@execute sender.send("&cプレイヤーからのみ実行出来るコマンドです")
+                sender.send("&f現在のポイントは &a${player.craftPointCounter.points} &fです")
             }
         }
     }
